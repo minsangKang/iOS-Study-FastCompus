68bfe02ac21f9f76d48a0d9f42aa8a38f503a100
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-02-07 17:23:17 +0900

    2/7 02 첫앱 만들기 - 15 중복내용 func 묶기

--- study1_album/study1_album/ViewController.swift
+++ study1_album/study1_album/ViewController.swift
@@ -10,24 +10,30 @@ import UIKit
 class ViewController: UIViewController {
     
     var currentValue = 0
-
+    
+    @IBOutlet var priceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        refresh()
     }
 
     @IBAction func hello(_ sender: Any) {
-        //string interpolation 기능
+        
         let message = "가격은 ₩\(currentValue) 입니다"
         let alert = UIAlertController(title: "Hello", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
-        
+        refresh()
+    }
+    
+    func refresh() {
         //랜덤숫자 생성 기능
         let randomPrice = arc4random_uniform(10000) + 1
         currentValue = Int(randomPrice)
+        //string interpolation 기능
+        priceLabel.text = "₩\(currentValue)"
     }
-    
 }
 
