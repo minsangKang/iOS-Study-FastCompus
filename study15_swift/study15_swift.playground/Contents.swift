9d6b3413c2b9beb5c6e329f71e69c64244d5b754
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-02-15 19:03:43 +0900

    2/15 07 스위프트 Structure 03 Structure 도전문제

---
+++ study15_swift/study15_swift.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+/* 도전과제 */
+//1. 강의 이름, 강사 이름, 학생수를 가지는 struct 만들기 (Lecture)
+struct Lecture {
+    let name: String
+    let instructor: String
+    let studentsNumber: Int
+}
+
+//2. 강의 array와 강사 이름을 받아서 해당 강사의 강의 이름을 출력하는 함수 만들기
+func printLectureName(lects: [Lecture], instructor: String) {
+    //1. 일반적인 for문을 통한 방법
+    var result = ""
+    for lecture in lects {
+        if lecture.instructor == instructor {
+            result = lecture.name
+        }
+    }
+    print("--> Lecture Name : \(result)")
+    
+    //2. closure, optional 사용한 방법
+    let lectureName = lects.first { lec in
+        return lec.instructor == instructor
+    }?.name ?? ""
+    print("--> Lecture Name : \(lectureName)")
+}
+
+//3. 강의 3개를 만들고 강사 이름으로 강의 찾기
+let Lecture1 = Lecture(name: "iOSStudy", instructor: "FastCampus", studentsNumber: 500)
+let Lecture2 = Lecture(name: "iOS", instructor: "FC", studentsNumber: 500)
+let Lecture3 = Lecture(name: "ios", instructor: "fc", studentsNumber: 500)
+let Lectures = [Lecture1, Lecture2, Lecture3]
+
+printLectureName(lects: Lectures, instructor: "FC")
