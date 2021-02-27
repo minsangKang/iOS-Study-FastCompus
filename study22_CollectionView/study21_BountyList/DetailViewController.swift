afc8fe9a67f3b072aacbfd64afe3c2c6a7e7b7bd
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-02-27 10:27:50 +0900

    2/27 11 원피스앱 06 Animation 추가 완료

--- study22_CollectionView/study21_BountyList/DetailViewController.swift
+++ study22_CollectionView/study21_BountyList/DetailViewController.swift
@@ -28,11 +28,21 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var bountyLabel: UILabel!
 
-    let viewModel = DetailViewModel()
+    @IBOutlet weak var nameLabelCenterX: NSLayoutConstraint!
+    @IBOutlet weak var bountyLabelCenterX: NSLayoutConstraint!
     
+    let viewModel = DetailViewModel()
+    //view가 보여기지 전
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
+        prepareAnimation()
+    }
+    //view가 보여진 후
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //animation 추가
+        showAnimation()
     }
     
     func updateUI() {
@@ -42,6 +52,41 @@ class DetailViewController: UIViewController {
             nameLabel.text = bountyInfo.name
             bountyLabel.text = "\(bountyInfo.bounty)"
         }
+    }
+    
+    private func prepareAnimation() {
+        //constant 값을 수정하여 이동한다
+        nameLabelCenterX.constant = view.bounds.width
+        bountyLabelCenterX.constant = view.bounds.width
+    }
+    
+    private func showAnimation() {
+        nameLabelCenterX.constant = 0
+        bountyLabelCenterX.constant = 0
+//        //초간단 애니메이션
+//        UIView.animate(withDuration: 0.3) {
+//            self.view.layoutIfNeeded()
+//        }
+//        UIView.animate(withDuration: 0.3,
+//                       delay: 0.1,
+//                       options: .curveEaseIn,
+//                       animations: {
+//            self.view.layoutIfNeeded()
+//        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.5,
+                       delay: 0.2,
+                       usingSpringWithDamping: 0.6,
+                       initialSpringVelocity: 2,
+                       options: .allowUserInteraction,
+                       animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+        
+        UIView.transition(with: imgView,
+                          duration: 0.3,
+                          options: .transitionFlipFromLeft,
+                          animations: nil, completion: nil)
     }
     
     @IBAction func close(_ sender: Any) {
