745b53d775296ff7e542fdeb121a22ce07adff65
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-02-27 10:52:16 +0900

    2/27 11 원피스앱 Animation 추가내용 끝

--- study22_CollectionView/study21_BountyList/DetailViewController.swift
+++ study22_CollectionView/study21_BountyList/DetailViewController.swift
@@ -55,38 +55,29 @@ class DetailViewController: UIViewController {
     }
     
     private func prepareAnimation() {
-        //constant 값을 수정하여 이동한다
-        nameLabelCenterX.constant = view.bounds.width
-        bountyLabelCenterX.constant = view.bounds.width
+        nameLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0)
+            .scaledBy(x: 3, y: 3).rotated(by: 180)
+        bountyLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0)
+            .scaledBy(x: 3, y: 3).rotated(by: 180)
+        
+        nameLabel.alpha = 0
+        bountyLabel.alpha = 0
     }
     
     private func showAnimation() {
         nameLabelCenterX.constant = 0
         bountyLabelCenterX.constant = 0
-//        //초간단 애니메이션
-//        UIView.animate(withDuration: 0.3) {
-//            self.view.layoutIfNeeded()
-//        }
-//        UIView.animate(withDuration: 0.3,
-//                       delay: 0.1,
-//                       options: .curveEaseIn,
-//                       animations: {
-//            self.view.layoutIfNeeded()
-//        }, completion: nil)
-        
-        UIView.animate(withDuration: 0.5,
-                       delay: 0.2,
-                       usingSpringWithDamping: 0.6,
-                       initialSpringVelocity: 2,
-                       options: .allowUserInteraction,
-                       animations: {
-            self.view.layoutIfNeeded()
+
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+            self.nameLabel.transform = CGAffineTransform.identity
+            self.nameLabel.alpha = 1
+        }, completion: nil)
+        UIView.animate(withDuration: 1, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+            self.bountyLabel.transform = CGAffineTransform.identity
+            self.bountyLabel.alpha = 1
         }, completion: nil)
         
-        UIView.transition(with: imgView,
-                          duration: 0.3,
-                          options: .transitionFlipFromLeft,
-                          animations: nil, completion: nil)
+        UIView.transition(with: imgView, duration: 0.5, options: .transitionFlipFromLeft, animations: nil, completion: nil)
     }
     
     @IBAction func close(_ sender: Any) {
