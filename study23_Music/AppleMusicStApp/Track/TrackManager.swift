f564b54e9a2152aeb440a867283c4e39cdb02a6c
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-03-04 00:06:13 +0900

    3/4 12 애플뮤직 05 트랙메니저 작성(난이도 상)

--- study23_Music/AppleMusicStApp/Track/TrackManager.swift
+++ study23_Music/AppleMusicStApp/Track/TrackManager.swift
@@ -17,27 +17,69 @@ class TrackManager {
     
     // TODO: 생성자 정의하기
     init() {
-        //재생을 위한 장치 : URL로 접근이 가능하다
-//        let playerItem = AVPlayerItem(url: URL)
+        self.tracks = loadTracks()
+        self.albums = loadAlbums(tracks: tracks)
+        self.todaysTrack = self.tracks.randomElement()
     }
 
     // TODO: 트랙 로드하기
     func loadTracks() -> [AVPlayerItem] {
-        return []
+        //파일들 읽어서 AVPlayerItem 만들기
+        //앱 : Bundle이므로, Bundle 안의 mp3 파일을 찾는식
+        let urls = Bundle.main.urls(forResourcesWithExtension: "mp3", subdirectory: nil) ?? []
+        //방법 1
+//        var items: [AVPlayerItem] = []
+//
+//        for url in urls {
+//            let item = AVPlayerItem(url: url)
+//            items.append(item)
+//        }
+        
+        //방법 2
+//        let items = urls.map { url -> AVPlayerItem in
+//            let item = AVPlayerItem(url: url)
+//            return item
+//        }
+        
+        //방법 3
+        let items = urls.map { url in
+            return AVPlayerItem(url: url)
+        }
+        
+        //방법 4
+//        let items = urls.map { AVPlayerItem(url: $0) }
+        
+        
+        return items
     }
     
     // TODO: 인덱스에 맞는 트랙 로드하기
     func track(at index: Int) -> Track? {
-        return nil
+        let playerItem = tracks[index]
+        //AVPlayerItem -> Track으로 변환이 필요하다
+        let track = playerItem.convertToTrack()
+        return track
     }
 
     // TODO: 앨범 로딩메소드 구현
     func loadAlbums(tracks: [AVPlayerItem]) -> [Album] {
-        return []
+        //AVPlayerItem -> Track으로 변환하여 배열에 추가
+        let trackList: [Track] = tracks.compactMap { $0.convertToTrack() }
+        //key : track.albumName, value : trackList 에서 key 값에 해당하는 값들
+        let albumDics = Dictionary(grouping: trackList, by: { (track) in track.albumName })
+        var albums: [Album] = []
+        
+        for (key, value) in albumDics {
+            let title = key
+            let tracks = value
+            let album = Album(title: title, tracks: tracks)
+            albums.append(album)
+        }
+        return albums
     }
 
     // TODO: 오늘의 트랙 랜덤으로 선책
     func loadOtherTodaysTrack() {
-        
+        self.todaysTrack = self.tracks.randomElement()
     }
 }
