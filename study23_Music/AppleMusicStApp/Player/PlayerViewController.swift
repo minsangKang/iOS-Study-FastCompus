c8a62070da4280a3305c49bb62c61cf9f6bb3dd3
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-03-07 23:30:38 +0900

    3/7 12 애플뮤직 13 곡정보 띄우기, play, pause 실행 성공

--- study23_Music/AppleMusicStApp/Player/PlayerViewController.swift
+++ study23_Music/AppleMusicStApp/Player/PlayerViewController.swift
@@ -34,7 +34,7 @@ class PlayerViewController: UIViewController {
         updateTime(time: CMTime.zero)
         // TODO: TimeObserver 구현
     }
-    
+    //view가 보이기 직전
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         updateTintColor()
@@ -61,7 +61,11 @@ class PlayerViewController: UIViewController {
     
     @IBAction func togglePlayButton(_ sender: UIButton) {
         // TODO: 플레이버튼 토글 구현
-        
+        if simplePlayer.isPlaying {
+            simplePlayer.pause()
+        } else {
+            simplePlayer.play()
+        }
         updatePlayButton()
     }
 }
@@ -69,7 +73,10 @@ class PlayerViewController: UIViewController {
 extension PlayerViewController {
     func updateTrackInfo() {
         // TODO: 트랙 정보 업데이트
-        
+        guard let track = simplePlayer.currentItem?.convertToTrack() else { return }
+        thumbnailImageView.image = track.artwork
+        titleLabel.text = track.title
+        artistLabel.text = track.artist
     }
     
     func updateTintColor() {
@@ -102,5 +109,14 @@ extension PlayerViewController {
     
     func updatePlayButton() {
         // TODO: 플레이버튼 업데이트 UI작업 > 재생/멈춤
+        if simplePlayer.isPlaying {
+            let configuration = UIImage.SymbolConfiguration(pointSize: 40)
+            let image = UIImage(systemName: "pause.fill", withConfiguration: configuration)
+            playControlButton.setImage(image, for: .normal)
+        } else {
+            let configuration = UIImage.SymbolConfiguration(pointSize: 40)
+            let image = UIImage(systemName: "play.fill", withConfiguration: configuration)
+            playControlButton.setImage(image, for: .normal)
+        }
     }
 }
