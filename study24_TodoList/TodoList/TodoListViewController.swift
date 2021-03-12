4ac440d2a65aadfbd04b0c8f56a2c4ea6882bb55
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-03-12 17:08:49 +0900

    3/12 13 Todolist 10 input View 생성, 연결, 구현 성공

--- study24_TodoList/TodoList/TodoListViewController.swift
+++ study24_TodoList/TodoList/TodoListViewController.swift
@@ -31,19 +31,19 @@ class TodoListViewController: UIViewController {
         // TODO: 데이터 불러오기 : OK
         todoListViewModel.loadTasks()
         
-        let todo = TodoManager.shared.createTodo(detail: "👍 🚀 Corona 난리", isToday: true)
-        Storage.saveTodo(todo, fileName: "test.json")
+//        let todo = TodoManager.shared.createTodo(detail: "👍 🚀 Corona 난리", isToday: true)
+//        Storage.saveTodo(todo, fileName: "test.json")
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
-        let todo = Storage.restoreTodo("test.json")
-        print("--> restore frome disk: \(todo)")
+//        let todo = Storage.restoreTodo("test.json")
+//        print("--> restore frome disk: \(todo)")
     }
     
     @IBAction func isTodayButtonTapped(_ sender: Any) {
         // TODO: 투데이 버튼 토글 작업
-        
+        isTodayButton.isSelected = !isTodayButton.isSelected
     }
     
     @IBAction func addTaskButtonTapped(_ sender: Any) {
