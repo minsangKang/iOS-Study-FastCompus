3be2d8476c5625a06d8cf11369ee241e77bef990
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-03-14 19:11:29 +0900

    3/14 15 Netflix 03 search 버튼 클릭시 sesstion 실행여부 확인

--- study28_netflix/MyNetflix/SearchViewController.swift
+++ study28_netflix/MyNetflix/SearchViewController.swift
@@ -32,8 +32,64 @@ extension SearchViewController: UISearchBarDelegate {
         dismissKeyboard()
         //검색어가 있는지 확인
         guard let searchTerm = searchBar.text, searchTerm.isEmpty == false else { return }
-        // 네트워킹을 통한 검색
+        //네트워킹을 통한 검색
+        //- 서치텀을 가지고 네트워킹을 통해서 영화 검색
+        //- 검색API 필요 : OK
+        //- 결과를 받아올 모델 Movie, Response 필요 : OK
+        //- 결과를 받아와서 CollectionView 로 표현해주자
+        SearchAPI.search(searchTerm) { movies in
+            //CollectionView 로 표현해주자
+        }
         
         print("--> 검색어: \(searchTerm)")
     }
+}
+
+
+//검색API 제작
+class SearchAPI {
+    //@escaping : completion handler : closure가 method 밖에서 실행될 수 있게 설정
+    //static method의 이유 : SearchAPI.search() 처럼 바로 사용이 가능하기 때문
+    static func search(_ term: String, completion: @escaping ([Movie]) -> Void) {
+        let session = URLSession(configuration: .default)
+        let requestURL = getURL(term: term)
+        let dataTask = session.dataTask(with: requestURL) { data, response, error in
+            let successRange = 200..<300
+            guard error == nil,
+                let statusCode = (response as? HTTPURLResponse)?.statusCode,
+                successRange.contains(statusCode) else {
+                completion([])
+                return
+            }
+            guard let resultData = data else {
+                completion([])
+                return
+            }
+            //data -> [Movie]
+            let string = String(data: resultData, encoding: .utf8)
+            print("--> string: \(string)")
+        }
+        dataTask.resume()
+    }
+    
+    static func getURL(term: String) -> URL {
+        var urlComponents = URLComponents(string: "https://itunes.apple.com/search?")!
+        let mediaQuery = URLQueryItem(name: "media", value: "movie")
+        let entityQuery = URLQueryItem(name: "entity", value: "movie")
+        let termQuery = URLQueryItem(name: "term", value: term)
+        urlComponents.queryItems?.append(mediaQuery)
+        urlComponents.queryItems?.append(entityQuery)
+        urlComponents.queryItems?.append(termQuery)
+
+        return urlComponents.url!
+    }
+}
+
+//결과를 받아올 모델
+struct Response {
+    
+}
+
+struct Movie {
+    
 }
