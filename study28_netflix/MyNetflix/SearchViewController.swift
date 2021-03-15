87746f9cae4fb1b3218301bfbad77225404e8f03
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-03-15 22:25:08 +0900

    3/15 15 Nexfix 04 resultData -> decoder -> [Movie] -> closure 전달 완료

--- study28_netflix/MyNetflix/SearchViewController.swift
+++ study28_netflix/MyNetflix/SearchViewController.swift
@@ -37,8 +37,11 @@ extension SearchViewController: UISearchBarDelegate {
         //- 검색API 필요 : OK
         //- 결과를 받아올 모델 Movie, Response 필요 : OK
         //- 결과를 받아와서 CollectionView 로 표현해주자
+        
+        // search 함수에서 url -> decode -> [Movie]를 넘겨받아온 후의 closure
         SearchAPI.search(searchTerm) { movies in
             //CollectionView 로 표현해주자
+            print("--> in closure count: \(movies.count)")
         }
         
         print("--> 검색어: \(searchTerm)")
@@ -51,27 +54,32 @@ class SearchAPI {
     //@escaping : completion handler : closure가 method 밖에서 실행될 수 있게 설정
     //static method의 이유 : SearchAPI.search() 처럼 바로 사용이 가능하기 때문
     static func search(_ term: String, completion: @escaping ([Movie]) -> Void) {
+        //session, url 준비
         let session = URLSession(configuration: .default)
         let requestURL = getURL(term: term)
+        //task 실행
         let dataTask = session.dataTask(with: requestURL) { data, response, error in
             let successRange = 200..<300
+            //error 및 success 숫자 확인
             guard error == nil,
                 let statusCode = (response as? HTTPURLResponse)?.statusCode,
                 successRange.contains(statusCode) else {
                 completion([])
                 return
             }
+            //resultData로 저장
             guard let resultData = data else {
                 completion([])
                 return
             }
-            //data -> [Movie]
-            let string = String(data: resultData, encoding: .utf8)
-            print("--> string: \(string)")
+            //resultData -> [Movie]
+            let movies = parseMovies(resultData)
+            print("--> movies count: \(movies.count)")
+            completion(movies)
         }
         dataTask.resume()
     }
-    
+    //url components 구성
     static func getURL(term: String) -> URL {
         var urlComponents = URLComponents(string: "https://itunes.apple.com/search?")!
         let mediaQuery = URLQueryItem(name: "media", value: "movie")
@@ -83,13 +91,41 @@ class SearchAPI {
 
         return urlComponents.url!
     }
+    //resultData -> decode -> [Movies]
+    static func parseMovies(_ data: Data) -> [Movie] {
+        let decoder = JSONDecoder()
+        do {
+            let response = try decoder.decode(Response.self, from: data)
+            let movies = response.movies
+            return movies
+        } catch let error {
+            print(error.localizedDescription)
+            return[]
+        }
+    }
 }
 
 //결과를 받아올 모델
-struct Response {
+struct Response: Codable {
+    let resultCount: Int
+    let movies: [Movie]
     
+    enum CodingKeys: String, CodingKey {
+        case resultCount
+        case movies = "results"
+    }
 }
 
-struct Movie {
+struct Movie: Codable {
+    let title: String
+    let director: String
+    let thumbnailPath: String
+    let previewURL: String
     
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case director = "artistName"
+        case thumbnailPath = "artworkUrl100"
+        case previewURL = "previewUrl"
+    }
 }
