4344d3c48006842afd47df89c4243b6d1caae5ce
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-03-25 18:49:39 +0900

    3/25 16 Firebase 07 기본타입 저장

--- study29_firebase/study29_firebase/ViewController.swift
+++ study29_firebase/study29_firebase/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         updateLabel()
+        saveBasicTypes()
+        saveCustomers()
     }
     
     func updateLabel() {
@@ -27,7 +29,53 @@ class ViewController: UIViewController {
             }
         }
     }
-
-
 }
 
+extension ViewController {
+    func saveBasicTypes() {
+        let url = db.child("basicTypes")
+        url.child("int").setValue(3)
+        url.child("double").setValue(3.5)
+        url.child("string").setValue("string value - 여러분 안녕")
+        url.child("array").setValue(["a", "b", "c"])
+        url.child("dictionary").setValue(["id": "anyID", "age": 10, "city": "seoul"])
+    }
+    
+    func saveCustomers() {
+        let books = [Book(title: "Good to Great", author: "Someone"), Book(title: "Hacking Growth", author: "Somebody")]
+        let customer1 = Customer(id: "\(Customer.id)", name: "Son", books: books)
+        Customer.id += 1
+        let customer2 = Customer(id: "\(Customer.id)", name: "Dele", books: books)
+        Customer.id += 1
+        let customer3 = Customer(id: "\(Customer.id)", name: "Kane", books: books)
+        Customer.id += 1
+        
+        let url = db.child("customers")
+        url.child(customer1.id).setValue(customer1.toDictionary)
+        url.child(customer2.id).setValue(customer2.toDictionary)
+        url.child(customer3.id).setValue(customer3.toDictionary)
+    }
+}
+
+struct Customer {
+    let id: String
+    let name: String
+    let books: [Book]
+    static var id: Int = 0
+    
+    var toDictionary: [String: Any] {
+        let booksArray = books.map { $0.toDictionary }
+        let dict: [String: Any] = ["id": id, "name": name, "books": booksArray]
+        return dict
+    }
+}
+
+struct Book {
+    let title: String
+    let author: String
+    
+    var toDictionary: [String: Any] {
+        let dict: [String: Any] = ["title": title, "author": author]
+        return dict
+    }
+}
